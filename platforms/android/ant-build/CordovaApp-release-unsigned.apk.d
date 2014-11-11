9000142c7d8bb28f117abb341b2f8136bbeec854
Alberto Bermejo Gil <abermejo@aubay.es>
2014-11-11 10:41:48 +0100

    Initial commit

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\abermejo\workspace\android\Martorela\platforms\android\ant-build\CordovaApp-release-unsigned.apk : \
+C:\Users\abermejo\workspace\android\Martorela\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\abermejo\workspace\android\Martorela\platforms\android\ant-build\classes.dex \
+C:\Users\abermejo\workspace\android\Martorela\platforms\android\CordovaLib\ant-build\classes.jar \
